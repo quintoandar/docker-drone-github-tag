53fdd0568b9d17b49265ce04da1e4416476a29f9
Fernando Barbosa <fernando.barbosa@quintoandar.com.br>
2018-01-15 12:01:03 -0200

    Update plugin to accept api_key as env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,26 @@ git tag ${PLUGIN_VERSION}
 
 if [ -n "$GITHUB_TOKEN" ]; then
     git push https://${GITHUB_TOKEN}@github.com/${DRONE_REPO_OWNER}/${DRONE_REPO_NAME} ${PLUGIN_VERSION}
-elif [ -f "$PLUGIN_API_KEY" ]; then
+
+elif [ -n "$PLUGIN_API_KEY" ]
     mkdir -p /root/.ssh
-    chmod 600 $PLUGIN_API_KEY
     touch /root/.ssh/known_hosts
     chmod 600 /root/.ssh/known_hosts
-    eval "$(ssh-agent -s)"
-    ssh-add $PLUGIN_API_KEY
     ssh-keyscan -H github.com > /etc/ssh/ssh_known_hosts 2> /dev/null
+    eval "$(ssh-agent -s)"
+
+    # in case it is a file path
+    if [ -f "$PLUGIN_API_KEY" ]; then
+         cp $PLUGIN_API_KEY /root/.ssh/id_rsa
+    else
+         echo $PLUGIN_API_KEY > /root/.ssh/id_rsa
+    fi
+
+    chmod 600 /root/.ssh/id_rsa
+    ssh-add /root/.ssh/id_rsa
     git push git@github.com:${DRONE_REPO_OWNER}/${DRONE_REPO_NAME} ${PLUGIN_VERSION}
+
 else
     echo "Missing GitHub Token or API Key file!" && exit 1
+
 fi
